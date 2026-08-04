f0ce197db6c707676f1922ebbf9ee0078ecf765c
mattc3046 <mattc3046@gmail.com>
2026-08-03 22:27:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Replace the value below with the Client ID from your Spotify Developer app.
 window.SPOTIFY_CONFIG = {
-  clientId: "PASTE_YOUR_SPOTIFY_CLIENT_ID_HERE"
+  clientId: "5493124ec1ed4a59a8e3eba7800d73cb"
 };
